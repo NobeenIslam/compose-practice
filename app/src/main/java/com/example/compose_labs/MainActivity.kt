6892c72e8f027617c3caa3ded8f15fb059534db8
Nobeen Islam <nobeen.islam@xdesign.com>
2023-01-16 17:43:05 +0000

    Made a basic navigation to a a page

--- app/src/main/java/com/example/compose_labs/MainActivity.kt
+++ app/src/main/java/com/example/compose_labs/MainActivity.kt
@@ -2,13 +2,17 @@ package com.example.compose_labs
 
 import android.annotation.SuppressLint
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.compose_labs.components.Greeting
+import com.example.compose_labs.components.OnboardingScreen
 import com.example.compose_labs.ui.theme.ComposelabsTheme
 
 class MainActivity : ComponentActivity() {
@@ -24,14 +28,20 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MyApp(modifier: Modifier = Modifier){
-    var nickNames = listOf("Test","Stest","Test","test")
+    var navigateToScreen = remember { mutableStateOf(false) };
+
+    if(navigateToScreen.value){
+        var nickNames = listOf("Test","Test","Test","test");
 
         Column(){
             for (nickName in nickNames){
                 Greeting(nickName)
             }
         }
+    }else{
+        OnboardingScreen(message = "Welcome to the best app ever", onClickFn = { navigateToScreen.value = true;         Log.d("debug", "${navigateToScreen.value}") })
 
+    }
 }
 
 
--- app/src/main/java/com/example/compose_labs/components/OnboardingScreen.kt
+++ app/src/main/java/com/example/compose_labs/components/OnboardingScreen.kt
@@ -3,6 +3,8 @@ package com.example.compose_labs.components
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -11,13 +13,17 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.compose_labs.ui.theme.ComposelabsTheme
 
 @Composable
-fun OnboardingScreen(message:String){
+fun OnboardingScreen(message:String, onClickFn: ()-> Unit){
     Surface(color = Color.White, modifier = Modifier.fillMaxSize()){
         Column(verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally){
-            Text(text = message, textAlign = TextAlign.Center, color = Color.Black)
+            Text(text = message, textAlign = TextAlign.Center, color = Color.Black, modifier = Modifier.padding(bottom = 3.dp))
+            Button(onClick = { onClickFn() },) {
+               Text("Enter the dungeon")
+            }
         }
     }
 }
@@ -27,6 +33,6 @@ fun OnboardingScreen(message:String){
 @Composable
 fun OnboardingPreview() {
     ComposelabsTheme {
-        OnboardingScreen("Welcome to Compose Labs")
+        OnboardingScreen("Welcome to the most awesome app ever", onClickFn = { })
     }
 }
